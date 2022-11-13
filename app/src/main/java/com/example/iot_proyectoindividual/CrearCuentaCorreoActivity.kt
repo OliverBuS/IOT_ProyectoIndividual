ecc3f46e80d32e80b9fa49795384a9a7378dff4f
Oliver <oliver.bustamante@pucp.edu.pe>
2022-11-13 18:06:50 -0500

    Se terminaron las vistas del flujo de sesión

---
+++ app/src/main/java/com/example/iot_proyectoindividual/CrearCuentaCorreoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.iot_proyectoindividual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearCuentaCorreoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_cuenta_correo)
+    }
+}
---
+++ app/src/main/java/com/example/iot_proyectoindividual/RecuperarCuentaFinActivity.kt
@@ -0,0 +1,11 @@
+package com.example.iot_proyectoindividual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecuperarCuentaFinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recuperar_cuenta_fin)
+    }
+}
